f33dfac163573284c671ce4ba23f1b3c2f95bf38
Viacheslav Mykhailov <viacheslav.mykhailov@ev.lt>
2024-08-11 12:07:55 +0300

    add clear.py

---
+++ built-in functions/clear.py
@@ -0,0 +1,12 @@
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+list1 = [1, 2, 3]
+set1 = {'a', 'b', 'c'}
+frozenset1 = frozenset(list1)
+
+print(dict1)
+print(list1)
+print(set1)
+print(frozenset1)
+print(dict1.clear())
+print(list1.clear())
+print(set1.clear())
