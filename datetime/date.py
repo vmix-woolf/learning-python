e922e5524a13777eabad92d1e30ed70b5595f924
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-09-23 11:26:37 +0300

    add datetime directory

---
+++ datetime/date.py
@@ -0,0 +1,8 @@
+import datetime as dt
+
+# date = dt.date(2024, 6, 24)
+# date = dt.datetime(2024, 8, 25, 12, 30, 45, 1000)
+# date = dt.datetime(day=25, month=8, year=2024)
+# date = dt.time(hour=6, minute=30, second=45, microsecond=1000)
+date = dt.time()
+print(date)
