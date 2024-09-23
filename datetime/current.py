2e3d86938da9b40548d0c663e7ab7572d8307f0a
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-09-23 16:15:55 +0300

    add current.py with learning examples

---
+++ datetime/current.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+current_datetime = datetime.now()
+print(current_datetime)
+print(type(current_datetime))
+print(current_datetime.year)
+print(current_datetime.month)
+print(current_datetime.day)
+print(current_datetime.hour)
+print(current_datetime.minute)
+print(current_datetime.second)
+print(current_datetime.microsecond)
+print(current_datetime.tzinfo)
+
+print(type(current_datetime.date()))
+print(type(current_datetime.time()))
+current_date = current_datetime.date()
+current_time = current_datetime.time()
+print(datetime.combine(current_date, current_time))
