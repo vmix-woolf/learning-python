5e5b403acba86c8ed7b049e44db9e88714c03c25
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-09-24 10:55:27 +0300

    add burning man(striked out) Moscow

---
+++ datetime/moscow-burning.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+# Встановлення дати спалення Москви Наполеоном (14 вересня 1812 року)
+napoleon_burns_moscow = datetime(year=1812, month=9, day=14)
+
+# Поточна дата
+current_date = datetime.now()
+
+# Розрахунок кількості днів
+days_since = current_date.toordinal() - napoleon_burns_moscow.toordinal()
+print(days_since)
+
