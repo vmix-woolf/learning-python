3e1d3e0e074ac64424d5b24603141e666adcb9d1
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-09-25 11:28:14 +0300

    add math operations

---
+++ math/round-off.py
@@ -0,0 +1,12 @@
+import math
+
+number = 23.45
+print(math.ceil(number))
+print(math.floor(number))
+print(math.trunc(number))
+
+print(math.sin(math.pi / 2))
+print(math.cos(math.pi / 2))
+print(math.tan(math.pi / 2))
+print(math.pow(2, 3))
+print(math.gcd(4, 6))
