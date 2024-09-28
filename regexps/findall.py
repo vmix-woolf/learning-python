6f73bf63f8a612748928a2570be236ce87d1663b
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-09-28 23:08:36 +0300

    some changes

--- regexps/findall.py
+++ regexps/findall.py
@@ -25,13 +25,4 @@ formatted_name = re.sub(pattern, replacement, file_name)
 
 print(formatted_name)  
 
-phone = """
-        Михайло Куліш: 050-171-1634
-        Вікторія Кущ: 063-134-1729
-        Оксана Гавриленко: 068-234-5612
-        """
-pattern = r"(\d{3})-(\d{3})-(\d{4})"
-replacement = r"(\1) \2-\3"
-formatted_phone = re.sub(pattern, replacement, phone)
 
-print(formatted_phone)
