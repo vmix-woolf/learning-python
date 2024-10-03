3e755d0b0caeab53aeb33204e9f82ff4988a82ca
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-10-03 12:52:30 +0300

    add virtual env

---
+++ virtual-env/test.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'Це червоний текст')
+print(Back.GREEN + 'Це текст на зеленому фоні')
+print(Style.RESET_ALL + 'Це звичайний текст після скидання стилю')
