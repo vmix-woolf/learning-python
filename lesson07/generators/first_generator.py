d34e12ae25b5550c6758413914a7443a95e70e63
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-10-07 17:04:32 +0300

    add generators

---
+++ lesson07/generators/first_generator.py
@@ -0,0 +1,11 @@
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+gen = my_generator()
+
+# Використання next()
+print(next(gen))  # Виведе 1
+print(next(gen))  # Виведе 2
+print(next(gen))  # Виведе 3
